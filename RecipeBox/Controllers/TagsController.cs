7c0f877de34978b5d859aa3b020a42ab87d5271b
ryandefea <ryan.defea@gmail.com>
2022-08-09 13:49:58 -0500

    add edit and delete for tags
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>
    Co-authored-by: Filmer Tolentino <filmer.tolentino@gmail.com>

--- RecipeBox/Controllers/TagsController.cs
+++ RecipeBox/Controllers/TagsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -19,6 +20,47 @@ namespace RecipeBox.Controllers
     {
       List<Tag> model = _db.Tags.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Tag tag)
+    {
+      _db.Tags.Add(tag);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+        var thisTag = _db.Tags
+            .Include(tag => tag.JoinEntities)
+            .ThenInclude(join => join.Recipe)
+            .FirstOrDefault(tag => tag.TagId == id);
+        return View(thisTag);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisTag = _db.Tags.FirstOrDefault(tag => tag.TagId ==id);
+      ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "RecipeName", "RecipeRating", "RecipeInstruction");
+      return View(thisTag);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Tag tag, int RecipeId)
+    {
+      if (RecipeId !=0)
+      {
+        _db.TagRecipe.Add(new TagRecipe() { RecipeId = RecipeId, TagId = tag.TagId });
+      }
+      _db.Entry(tag).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
 
 
